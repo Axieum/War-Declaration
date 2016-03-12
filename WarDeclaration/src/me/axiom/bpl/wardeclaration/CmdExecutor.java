e2623ffa8095bb5dcd94864d3b9f0b4298875dda
Axiom1999 <jonathan.hiles@hotmail.com>
2016-03-12 12:11:26 +1000

    Added the 'list' command.

--- WarDeclaration/src/me/axiom/bpl/wardeclaration/CmdExecutor.java
+++ WarDeclaration/src/me/axiom/bpl/wardeclaration/CmdExecutor.java
@@ -1,9 +1,15 @@
 package me.axiom.bpl.wardeclaration;
 
+import java.util.HashSet;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.FactionColl;
+import com.massivecraft.factions.entity.MPlayer;
 
 public class CmdExecutor implements CommandExecutor {
 
@@ -16,10 +22,18 @@ public class CmdExecutor implements CommandExecutor {
 	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
 		
 		boolean isPlayer = false;
+		boolean hasFaction = false;
+		MPlayer mp = null;
+		Faction f = null;
 		
 		// Declare whether the CommandSender is in-game or is in the console.
 		if (s instanceof Player) {
 			isPlayer = true;
+			mp = MPlayer.get(s);
+			if (mp.hasFaction()) {
+				hasFaction = true;
+				f = mp.getFaction();
+			}
 		}
 		
 		// "/war" COMMAND : HELP MENU.
@@ -50,7 +64,18 @@ public class CmdExecutor implements CommandExecutor {
 		
 		if (args.length == 1) { // They typed, "/war something".
 			if (args[0].equalsIgnoreCase("list")) { // They typed, "/war list".
-				
+				s.sendMessage("§6-------§c§lWar List§r§6-------");
+				for (Faction fa : FactionColl.get().getAll()) { // Gets every faction and runs the following code FOR EACH.
+					HashSet<String> factionsProcessed = new HashSet<String>(); // This will hold all factions already shown, stops duplicates.
+					if (plugin.factionsWar.getString(fa.getName()) != "none" && !(factionsProcessed.contains(fa.getName()))) { // Checks if the faction is at war.
+						if (f == fa) { // If the sender's faction is shown, make they're faction green.
+							s.sendMessage("§a" + fa.getName() + " §7<--[]--> §e" + plugin.factionsWar.getString(fa.getName()));
+						} else {
+							s.sendMessage("§e" + fa.getName() + " §7<--[]--> §e" + plugin.factionsWar.getString(fa.getName()));
+						}
+						factionsProcessed.add(plugin.factionsWar.getString(fa.getName()));
+					}
+				}
 			}
 		}
 		
